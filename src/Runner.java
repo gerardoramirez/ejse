34fb0b2db26e648e90fed842394a7cb72545217b
Martin Petzsch <mpetzsch@gmail.com>
2011-04-07 21:18:56 +0100

    first commit - idea structure and readme

---
+++ src/Runner.java
@@ -0,0 +1,12 @@
+/**
+ * Created by IntelliJ IDEA.
+ * User: mpetzsch
+ * Date: 06/04/2011
+ * Time: 22:32
+ * To change this template use File | Settings | File Templates.
+ */
+public class Runner {
+    public static void main(String[] args) {
+        System.out.println("This is a new test message from intellij idea 64bit on my macbook pro....");
+    }
+}
